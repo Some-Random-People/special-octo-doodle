7e138f55430b3120f8b8e0912e7811e8fecf7fd0
rysiek21 <59115878+rysiek21@users.noreply.github.com>
2023-01-21 00:46:12 +0100

    Test request to osu! API

--- cogs/test.py
+++ cogs/test.py
@@ -1,14 +1,21 @@
+import os
 import discord
+import requests
 from discord.ext import commands
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @discord.command(description="Sends the bot's latency.")
-    async def test(self, ctx):
-        await ctx.respond("Test successful!")
+    async def test(self, ctx, userid: str):
+        response = requests.get(f"https://osu.ppy.sh/api/v2/users/{userid}/",
+                                headers={"Authorization": f"Bearer {os.getenv('OSU_TOKEN')}"},
+                                )
+        await ctx.respond(response.json()["statistics"]["pp"])
 
 
 def setup(bot):
