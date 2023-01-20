c24595f02d386eee8cb24d9113bb9f27a5b38352
Master5111 <pawel9395@wp.pl>
2023-01-20 23:18:26 +0100

    Ready message

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from dotenv import load_dotenv, find_dotenv
 import discord
 
 load_dotenv(find_dotenv())
-
 bot = discord.Bot()
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
 
 bot.run(os.getenv('TOKEN'))
