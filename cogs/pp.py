290b7f72a36f22834b4321c16cb9b1be6c9183d1
Master5111 <pawel9395@wp.pl>
2023-01-21 22:38:24 +0100

    /Profile command

--- cogs/pp.py
+++ cogs/pp.py
@@ -18,6 +18,51 @@ class Pp(commands.Cog):
                                 )
         await ctx.respond(response.json()["statistics"]["pp"])
 
+    @discord.command(description = "Show profile")
+    async def profile(self, ctx, userid: str):
+        response = requests.get(f"https://osu.ppy.sh/api/v2/users/{userid}/",
+                                headers={"Authorization": f"Bearer {os.getenv('OSU_TOKEN')}"},
+                                )
+
+        if response.status_code == 404:
+            embed = discord.Embed(
+                title="Verification error",
+                description=f"User with user id {userid} not found.",
+                color=discord.Color.red()
+            )
+            await ctx.respond(embed=embed, ephemeral=True)
+            return
+
+        response = response.json()
+        if response["is_active"]:
+            active_status = ":green_circle:"
+        else:
+            active_status = ":red_circle:"
+        if response["is_online"]:
+            online_status = ":green_circle:"
+        else:
+            online_status = ":red_circle:"
+
+
+
+        embed = discord.Embed(
+            title=userid + "'s Profile",
+            description=f":flag_{response['country_code'].lower()}:",
+            color = discord.Colour.blurple(),
+        )
+        embed.add_field(name="Active", value=active_status, inline=True)
+        embed.add_field(name="Online", value=online_status, inline=True)
+        embed.add_field(name="", value="")
+        embed.add_field(name="Stats", value=f"Global Rank: {response['statistics']['global_rank']}"
+                                            f"\nCountry Rank: {response['statistics']['country_rank']}"
+                                            f"\nPP: {response['statistics']['pp']}"
+                                            f"\nPlaytime: {response['statistics']['play_time']//3600}h"
+                    )
+
+        embed.set_thumbnail(url=response["avatar_url"])
+
+        await ctx.respond("Hello! Here's a cool embed.", embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Pp(bot))
