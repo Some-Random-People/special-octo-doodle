ae1968ca6be153496cf22fbb822a8927bfbf3463
rysiek21 <59115878+rysiek21@users.noreply.github.com>
2023-01-21 14:34:26 +0100

    Timestamp for verify command

--- cogs/verify.py
+++ cogs/verify.py
@@ -4,6 +4,7 @@ import requests
 from discord.ext import commands
 from dotenv import load_dotenv, find_dotenv
 from Functions.roll_beatmap import roll
+from datetime import datetime, date, timedelta
 
 load_dotenv(find_dotenv())
 
@@ -31,13 +32,16 @@ class Verify(commands.Cog):
                                     )
         map_response = map_response.json()
         user_response = user_response.json()
+        to_add = datetime(date.today().year, date.today().month, date.today().day, 15, 0, 0)
+        timestamp = datetime.now() + timedelta(minutes=15)
+        timestamp = timestamp.strftime("%d/%m/%Y %H:%M:%S")
         embed = discord.Embed(
             title=f"Verification {user_response['username']}",
             description="You started the verification process.",
             color=discord.Color.green()
         )
-        embed.add_field(name="How to verify?", value="To verify you account you have to play map that is shown below. "
-                                                     "You have to do that in 15 minutes.", inline=False)
+        embed.add_field(name="How to verify?", value=f"To verify you account you have to play map that is shown below. "
+                                                     f"You have to do that in 15 minutes. {timestamp}", inline=False)
         embed.add_field(name="Map", value=f"[osu! beatmap website]({map_response['url']})"
                                           f"\nTitle: {map_response['beatmapset']['title']}"
                                           f"\nArtist: {map_response['beatmapset']['artist']}"
