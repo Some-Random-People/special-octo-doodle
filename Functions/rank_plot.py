74757f9b1e6b5632a22818a1020d9a91c550ac3c
Master5111 <pawel9395@wp.pl>
2023-01-22 00:42:16 +0100

    /Profile command update v2

--- Functions/rank_plot.py
+++ Functions/rank_plot.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 
 def plots(y):
     x = []
-    for i in range(1,91):
+    for i in range(90,0,-1):
         x.append(i)
-    y.reverse()
     plt.plot(x, y)
     plt.xlabel("")
     plt.ylabel("")
