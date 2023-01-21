e9ccb6cd648e184a7067354a474875f53187642a
Master5111 <pawel9395@wp.pl>
2023-01-21 12:56:24 +0100

    Roll beatmap

---
+++ Functions/roll_beatmap.py
@@ -0,0 +1,12 @@
+import random
+
+beat = []
+with open('../beatmap_list.txt', 'r') as x:
+    content = x.readlines()
+    x.close()
+
+def roll():
+    for i in content:
+        beat.append(i.strip("\n"))
+
+    return beat[random.randint(0,len(beat) - 1)]
